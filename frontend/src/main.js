f866b9a39d390a30bfd5d3027d69bc019e922a15
Sathwik Kallapur <satpk17@gmail.com>
2025-09-05 06:16:28 +1000

    make password prompt better

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,18 @@ import router from './router/index.js'
 
 const appName = import.meta.env.VITE_APP_NAME;
 
-createApp(App)
+// sessionStorage.setItem('access_granted', 'false')
+const accessGranted = sessionStorage.getItem('access_granted') === 'true'
+
+// const accessGranted = localStorage.getItem('access_granted') === 'true'
+
+if (accessGranted) {
+  createApp(App)
   .use(router)
   .provide('appName', appName)
   .mount('#app')
+} else {
+  import('./components/PasswordGate.vue').then(({ default: Gate }) => {
+    createApp(Gate).mount('#app')
+  })
+}
