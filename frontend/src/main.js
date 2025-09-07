f8f9a931ead261144f8e91b6039edfa037cc8a3d
Sathwik Kallapur <satpk17@gmail.com>
2025-09-07 17:57:18 +1000

    remove password page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,18 +5,7 @@ import router from './router/index.js'
 
 const appName = import.meta.env.VITE_APP_NAME;
 
-// sessionStorage.setItem('access_granted', 'false')
-const accessGranted = sessionStorage.getItem('access_granted') === 'true'
-
-// const accessGranted = localStorage.getItem('access_granted') === 'true'
-
-if (accessGranted) {
-  createApp(App)
-  .use(router)
-  .provide('appName', appName)
-  .mount('#app')
-} else {
-  import('./components/PasswordGate.vue').then(({ default: Gate }) => {
-    createApp(Gate).mount('#app')
-  })
-}
+createApp(App)
+.use(router)
+.provide('appName', appName)
+.mount('#app')
